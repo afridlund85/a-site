c382615842fe613671707c1cb3714f9da149d887
Andreas <afridlund85@gmail.com>
2018-03-04 18:29:17 +0100

    footer fix

--- src/Layout/Footer/components/Footer.jsx
+++ src/Layout/Footer/components/Footer.jsx
@@ -8,17 +8,17 @@ const Footer = () => (
       <div className="row social">
         <div className="column column-33">
           <a href="https://www.facebook.com/AlexanderHallPhoto" target="_blank">
-            <i className="fa fa-facebook-official" aria-hidden="true"></i>
+            <i className="fab fa-facebook fa-lg" aria-hidden="true"></i>
           </a>
         </div>
         <div className="column column-33">
           <a href="http://instagram.com/kaptenhall" target="_blank">
-            <i className="fa fa-instagram" aria-hidden="true"></i>
+            <i className="fab fa-instagram fa-lg" aria-hidden="true"></i>
           </a>
         </div>
         <div className="column column-33">
           <a href="http://alwayspreparedforadventure.tumblr.com/" target="_blank">
-            <i className="fa fa-tumblr-square" aria-hidden="true"></i>
+            <i className="fab fa-tumblr-square fa-lg" aria-hidden="true"></i>
           </a>
         </div>
       </div>
